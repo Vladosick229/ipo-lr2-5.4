4b951cc3cb5b34f4f6445a265a197516c15a11da
Vlad <vladsevruk8@gmail.com>
2024-09-26 15:53:38 +0300

    №5

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-octal_number = input("Введите число в восьмеричной системе счисления (5 чисел): ")
+numbers_input = input("Введите восьмиричное число через пробел: ")
 
-if len(octal_number) == 5:
-    decimal_number = int(octal_number, 8)
-    print(f"Число в десятичной системе счисления: {decimal_number}")
-else:
-    print("Ошибка: Введите число, состоящее из 5 разрядов.")
+n1 = int(numbers_input[0])
+n2 = int(numbers_input[1])
+n3 = int(numbers_input[2])
+n4 = int(numbers_input[3])
+n5 = int(numbers_input[4])
+
+res = (n1 * 8 ** 4) + (n2 * 8 ** 3) + (n3 * 8 ** 2) + (n4 * 8 ** 1) + (n5 * 8 ** 0)
+
+print(f"Число в десятичной системе счисления: {res}")
